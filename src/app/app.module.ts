a825f954b94eadc59e495e07c36add115c6152c5
Abhi <mailshine94@gmail.com>
2023-04-02 10:52:26 -0400

    updated links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HighlightCardComponent } from './highlight-card/highlight-card.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TerminalComponent } from './terminal/terminal.component';
+import { LinksComponent } from './links/links.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HighlightCardComponent,
+    ResourcesComponent,
+    SkillsComponent,
+    TerminalComponent,
+    LinksComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
